c90bf131da70b641c6c5adb50462c2000bf952c0
quewenjin <45279636+quewenjin@users.noreply.github.com>
2020-11-22 09:25:03 +0800

    update

---
+++ src/main/java/com/example/shiyouge/bean/Admin.java
@@ -0,0 +1,22 @@
+package com.example.shiyouge.bean;
+
+public class Admin {
+    private String adminId;//管理员账号
+    private String adminPassword;//管理员密码
+
+    public String getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(String adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
+    }
+}
--- src/main/java/com/example/shiyouge/service/AdminService.java
+++ src/main/java/com/example/shiyouge/service/AdminService.java
@@ -1,14 +1,13 @@
 package com.example.shiyouge.service;
 
-import com.example.shiyouge.mapper.ReportMapper;
+import com.example.shiyouge.mapper.AdminMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-public class ReportService {
+public class AdminService {
     @Autowired
-    ReportMapper reportMapper;
-
+    AdminMapper adminMapper;
 }
