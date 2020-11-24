fc4cc4cd35addcecf70bcdc089f6ca73f36f05c2
quewenjin <45279636+quewenjin@users.noreply.github.com>
2020-11-24 17:27:37 +0800

    update

--- src/main/java/com/example/shiyouge/controller/UserController.java
+++ src/main/java/com/example/shiyouge/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping(value = "/UserCenter")
+@RequestMapping(value = "/userCenter")
 public class UserController {
     @Autowired
     UserService userService;
